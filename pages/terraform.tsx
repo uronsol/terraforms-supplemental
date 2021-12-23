1b5b33b6d9282d24b27439b381d2f5e14c733b4f
uronsol <uronsolana@gmail.com>
2021-12-23 03:10:47 -0500

    Add terms and what not

--- pages/terraform.tsx
+++ pages/terraform.tsx
@@ -17,16 +17,6 @@ function Terraform() {
   return (
     <div className="flex flex-col items-center flex-grow pb-24">
       <h1 className="text-white text-3xl mb-4">Image to Terraform Tool</h1>
-      <p className="text-white-100 text-xl w-3/5 text-center mb-4">
-        To get the best image possible out of this tool multiply the height of
-        your artboard by 0.6369694844 to find it&apos;s width. For square images
-        you may want to adjust them due to the aspect ratio of the canvases.
-      </p>
-      <p className="text-white-100 text-xl w-3/5 text-center mb-8">
-        Note this tool is intended to help create a starting point for your art.
-        Copy the ASCII by clicking on the ASCII character and adjust the finer
-        details.
-      </p>
       {!fileTarget ? (
         <ImageInput
           onChange={(e) => {
@@ -56,8 +46,23 @@ function Terraform() {
           </div>
         ) : null}
       </div>
-      <p className="text-white-100 text-xl w-3/5 text-center mt-8">
-        Editing ASCII arrays with preview will be available soon.
+      <p className="text-white-100 text-xl w-3/5 text-center mb-8 mt-8">
+        To get the best image possible out of this tool multiply the height of
+        your artboard by 0.6369694844 to find it&apos;s width. For square images
+        you may want to adjust (squish y) them due to the aspect ratio of the
+        canvases.
+      </p>
+      <p className="text-white-100 text-xl w-3/5 text-center mb-8">
+        Note this tool is intended to help create a starting point for your art.
+        Copy the ASCII by clicking on the ASCII character and adjust the finer
+        details.
+      </p>
+      <p className="text-white-100 text-xl w-3/5 text-center mb-8">
+        I assume no responsibility for what you create with this tool, do with
+        your Terraforms, and I do not provide support.
+      </p>
+      <p className="text-white-100 text-xl w-3/5 text-center">
+        Editing ASCII arrays with preview will be available shortly.
       </p>
     </div>
   );
